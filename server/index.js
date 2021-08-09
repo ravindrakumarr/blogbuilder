89248286ca58039769cc839b78426b4e1cbfeea4
ravindrakumarr <40341496+ravindrakumarr@users.noreply.github.com>
2021-08-10 00:16:23 +0530

    package

--- server/index.js
+++ server/index.js
@@ -22,10 +22,10 @@ app.use('/users', userRoutes);
 app.use('/subscribers', subscriberRoutes);
 
 //this one is for mongo db atlas
-const CONNECTION_URL = "mongodb+srv://rkumar6821:tmkoc108@cluster0.o1ip5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+//const CONNECTION_URL = "mongodb+srv://rkumar6821:tmkoc108@cluster0.o1ip5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 //this is for local mongo db compass client
-//const CONNECTION_URL = "mongodb://localhost:27017/BlogBuilder"
+const CONNECTION_URL = "mongodb://localhost:27017/BlogBuilder"
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
